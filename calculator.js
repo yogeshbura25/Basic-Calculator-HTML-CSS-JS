ebf3695b0e5412ca3c993caa6092f5bb75766770
yogeshbura25 <burayogesh78@gmail.com>
2023-10-12 13:48:38 +0530

    Basic Calculator

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    const expression = document.getElementById('display').value;
+    const result = eval(expression);
+
+    if (isNaN(result)) {
+        alert("Invalid expression");
+    } else {
+        document.getElementById('display').value = result;
+    }
+}
